a5c989e0ef8ae294549b111615c95f3ee7ca1088
Stek00 <sterianmihaita@gmail.com>
2023-12-30 22:18:34 +0100

    filtri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,12 +15,12 @@ class PagesController < ApplicationController
     end
 
     def filters
-      @ristoranti = Ristorante.page(params[:page]).per(1)
+      @ristoranti = Ristorante.page(params[:page]).per(5)
       #@ristoranti_filter = @ristoranti.joins(:tipo_cucinas).where(["tipo_cucinas.id = ? and tipo_cucinas.nome = ?", "@ristoranti.id"])
       if params.has_key?(:type)
         @cucina = 'I migliori nella categoria '+params[:type].capitalize()
       else
-        @cucina = 'I ristoranti migliori'
+        @cucina = 'I migliori ristoranti'
         @dislay = @ristoranti
       end
 
